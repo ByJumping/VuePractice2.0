d39dac1a854910b4e8718d15859065bb2cf0b120
Aleksey Pravdin <leks16@bk.ru>
2021-07-27 16:58:40 +0300

    инлайн стили, вотчер

--- app.js
+++ app.js
@@ -23,6 +23,18 @@ const App = {
         removeNote(idx) {
             this.notes.splice(idx, 1)
         }
+    },
+    computed: {
+        doubleCount() {
+            return this.notes.length * 2
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = ''
+            }
+        }
     }
 }
 
