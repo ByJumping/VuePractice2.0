dc1e90e3bcc2de27415c0a5db0b9dd9b1e3b00e8
Aleksey Pravdin <leks16@bk.ru>
2021-08-09 14:34:24 +0300

    работа со слотами

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,80 +1,39 @@
 <template>
   <div class="container pt-1">
     <div class="card">
-      <h2>Актуальные новости {{now}}</h2>
-      <span>Открыто: <strong>{{ openRate }} </strong> | Прочитано: <strong>{{ readRate }}</strong></span>
+      <h2>Slots</h2>
     </div>
-    <app-news
-      v-for="item in news"
-      :key="item.id"
-      :title="item.title"
-      :id="item.id"
-      :is-open="item.isOpen"
-      :was-read="item.wasRead"
-      @unmark="unreadNews"
-      @open-news="openNews"
-      @read-news="readNews"
-    />
+
+    <app-list>
+      <template #default="{idx, iter}">
+      <span style="color: darkred">
+        <strong>{{ idx + 1 }}</strong>
+        Item: {{ iter }}</span>
+      </template>
+    </app-list>
+
+    <app-block>
+      <p>Это самый важный текст этого блока</p>
+      <template #header>
+        <h3>Это заголовок!</h3>
+      </template>
+      <template #footer>
+        <hr/>
+        Это футер
+      </template>
+    </app-block>
   </div>
 </template>
 
 <script>
-
-
-import AppNews from "./AppNews";
+import AppBlock from "./AppBlock";
+import AppList from "./AppList";
 
 export default {
-  name: 'App',
-  components: {
-    'app-news': AppNews
-  },
-  data () {
-    return {
-      openRate: 0,
-      readRate: 0,
-      now: new Date().toLocaleDateString(),
-      news: [
-        {
-          title: 'Джо байден победил на выборах в США',
-          id: 1,
-          isOpen: false,
-          wasRead: false
-        },
-        {
-          title: 'Vue 3 успешно работает',
-          id: 2,
-          isOpen: false,
-          wasRead: false
-        }
-      ]
-    }
-  },
-  provide() {
-    return {
-      title: 'Список всех новостей',
-      news: this.news
-    }
-  },
-  methods: {
-    readNews(id) {
-      const idx = this.news.findIndex(news=> news.id === id)
-      this.news[idx].wasRead = true
-      this.readRate++
-    },
-    openNews() {
-      this.openRate++
-    },
-    unreadNews(id) {
-      const news = this.news.find(news=> news.id === id)
-      news.wasRead = false
-      this.readRate--
-    }
-  }
+  components: {AppBlock, AppList}
 }
 </script>
 
 <style scoped>
-  h2 {
-    color: darkred;
-  }
+
 </style>
