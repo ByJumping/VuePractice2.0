b24131a0486d1c0a37f8afc9310a00a64989ed0d
Aleksey Pravdin <leks16@bk.ru>
2021-08-15 12:01:36 +0300

    миксины

---
+++ vue_mix/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+      <app-alert
+              v-if="alert"
+              title="Warning!"
+              text="Это очень важное сообщение"
+              type="danger"
+              closable
+              @close="alert = false"
+      ></app-alert>
+
+    <div class="card">
+        <button class="btn primary" @click="toggleAlert">{{alert ? 'Скрыть' : 'Показать'}} сообщение</button>
+    </div>
+
+      <app-block></app-block>
+  </div>
+</template>
+
+<script>
+
+import AppAlert from "./components/AppAlert";
+import AppBlock from "./components/AppBlock";
+import alertMixin from "./alertMixin";
+
+export default {
+    mixins: [alertMixin],
+    components: {
+        AppAlert,
+        AppBlock
+    }
+}
+</script>
+
