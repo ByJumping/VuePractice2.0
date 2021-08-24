8dd0ba9f6c2ca91c8d80e4d305964b195e4d9dd1
Aleksey Pravdin <leks16@bk.ru>
2021-08-24 21:39:31 +0300

    работа с vuex

---
+++ vue_vuex/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <the-navbar></the-navbar>
+    <div class="container with-nav">
+        <div class="card">
+            <h1>{{upperTitle}}</h1>
+            <h2>Счетчик {{ counter }} ({{ doubleCounter }})</h2>
+            <button class="btn primary" @click="add">Добавить</button>
+            <button class="btn danger" @click="incrementAsync({value: 10, delay: 1000})">Добавить 10</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import TheNavbar from './TheNavbar'
+import {mapGetters, mapMutations, mapActions} from 'vuex'
+
+export default {
+    components: {TheNavbar},
+    // computed: mapGetters(['counter', 'doubleCounter', 'upperTitle']),
+    computed: {
+        ...mapGetters(['upperTitle']),
+        ...mapGetters('count',['counter', 'doubleCounter'])
+    },
+    // data() {.0
+    //   return {
+    //       counter: 0
+    //   }
+    // },
+    methods: {
+        ...mapMutations({
+            add: 'count/increment'
+        }),
+        ...mapActions('count', ['incrementAsync'])
+        // incrementAsync() {
+        //     this.$store.dispatch('incrementAsync', {
+        //         value: 10,
+        //         delay: 2000
+        //     })
+        // }
+    },
+}
+</script>
---
+++ vue_vuex/src/TheNavbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="navbar">
+    Счетчик {{counter}}
+
+      <button class="btn" @click="add">Add 5</button>
+  </header>
+</template>
+
+<script>
+import {mapGetters, mapMutations} from 'vuex'
+
+  export default {
+    computed: {
+        // counter() {
+        //     return this.$store.getters.counter
+        // },
+        ...mapGetters('count', ['counter'])
+    },
+      methods: {
+        ...mapMutations({
+            addFive: 'count/add'
+        }),
+        // add() {
+        //     this.$store.commit('add', 5)
+        // }
+          add() {
+            this.addFive({value: 5})
+              // this.$store.commit({
+              //     type: 'add',
+              //     value: 5
+              // })
+          }
+      }
+  }
+</script>
